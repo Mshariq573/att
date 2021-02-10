f866544118363b23250c26869bee7e283bc0615d
Mshariq573 <78876731+Mshariq573@users.noreply.github.com>
2021-02-10 17:29:55 -0600

    Delete Object Repository directory

--- Object Repository/Bundles/ATT INTERNETWIRELESS_Text.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>ATT INTERNETWIRELESS_Text</name>
-   <tag></tag>
-   <elementGuidId>ff37580e-0ccb-4a79-92ff-a2985519b49b</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
